xen-troops/meta-xt-prod-devel

commit d8b048c11692d78de4c103b0a14fd346498188c8

Explicitly add XT_GUESTS_INSTALL variable into the recipe
Signed-off-by: Iurii Artemenko <iurii_artemenko@epam.com>

recipes-dom0/dom0-image-thin-initramfs/files/meta-xt-prod-extra/recipes-core/images/core-image-thin-initramfs.bbappend
@@ -12,6 +12,9 @@ IMAGE_INSTALL_append = " \
     domd-run \
     domd-install-artifacts \
 "
+
+XT_GUESTS_INSTALL = "doma domf"
+
 python __anonymous () {
     guests = d.getVar("XT_GUESTS_INSTALL", True).split()
     if "doma" in guests :

commit 963043e75fb6ae783ca2e27e44e0201932cae873

dom0: Fix installed domain artifacts
While checking for which domain artifacts must be installed
the default values must be weak, so one can override the
default settings.

Fixes: d8b048c11692 ("Explicitly add XT_GUESTS_INSTALL variable into the recipe")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

recipes-dom0/dom0-image-thin-initramfs/files/meta-xt-prod-extra/recipes-core/images/core-image-thin-initramfs.bbappend
@@ -15,7 +15,7 @@ IMAGE_INSTALL_append = " \
     domd-install-artifacts \
 "
 
-XT_GUESTS_INSTALL = "doma domf"
+XT_GUESTS_INSTALL ?= "doma domf"
 
 python __anonymous () {
     guests = d.getVar("XT_GUESTS_INSTALL", True).split()