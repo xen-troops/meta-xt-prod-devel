xen-troops/meta-xt-prod-devel

commit 8fe2a7da1c819e8c47df662ac0af098edb1c4f8d

Add Salvator-X M3 device trees to the kernel build
Add Salvator-X M3 device trees to the kernel build:
reuse DomA device tree from Salvator-X H3 ES3.0.

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

recipes-domd/domd-image-weston/files/meta-xt-prod-extra/recipes-kernel/linux/linux-renesas_%.bbappend
@@ -67,6 +67,23 @@ KERNEL_DEVICETREE_salvator-x-h3-xt = " \
     renesas/r8a7795-salvator-x-4x2g-doma.dtb \
 "
 
+##############################################################################
+# Salvator-X M3
+###############################################################################
+# N.B. DomA device tree is reused from Salvator-X H3 ES3.0 4x2G
+###############################################################################
+SRC_URI_append_salvator-x-m3-xt = " \
+    file://r8a7796-salvator-x-dom0.dts;subdir=git/arch/${ARCH}/boot/dts/renesas \
+    file://r8a7796-salvator-x-domd.dts;subdir=git/arch/${ARCH}/boot/dts/renesas \
+    file://r8a7795-salvator-x-4x2g-doma.dts;subdir=git/arch/${ARCH}/boot/dts/renesas \
+"
+
+KERNEL_DEVICETREE_salvator-x-m3-xt = " \
+    renesas/r8a7796-salvator-x-dom0.dts \
+    renesas/r8a7796-salvator-x-domd.dts \
+    renesas/r8a7795-salvator-x-4x2g-doma.dts \
+"
+
 do_deploy_append() {
     for DTB in ${KERNEL_DEVICETREE}
         do

commit 5d471df33e614014046dbd57413f9e6c70e24fd6

domd: Fix Salvator-X M3 device tree build
Device tree names must be provided as .dtb, not .dts,
so those are built with the kernel.

Fixes: 8fe2a7da1c81 ("Add Salvator-X M3 device trees to the kernel build")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

recipes-domd/domd-image-weston/files/meta-xt-prod-extra/recipes-kernel/linux/linux-renesas_%.bbappend
@@ -79,9 +79,9 @@ SRC_URI_append_salvator-x-m3-xt = " \
 "
 
 KERNEL_DEVICETREE_salvator-x-m3-xt = " \
-    renesas/r8a7796-salvator-x-dom0.dts \
-    renesas/r8a7796-salvator-x-domd.dts \
-    renesas/r8a7795-salvator-x-4x2g-doma.dts \
+    renesas/r8a7796-salvator-x-dom0.dtb \
+    renesas/r8a7796-salvator-x-domd.dtb \
+    renesas/r8a7795-salvator-x-4x2g-doma.dtb \
 "
 
 do_deploy_append() {