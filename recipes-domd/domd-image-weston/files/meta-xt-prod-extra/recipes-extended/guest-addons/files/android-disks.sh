6e00593636083f1479f932cf50d353c525c5cb50
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2018-07-06 11:51:04 +0300

    DomD: Change disks configuration for DomA
    DomA will have only one disk with GPT and
    all needed partitions inside.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- recipes-domd/domd-image-weston/files/meta-xt-prod-extra/recipes-extended/guest-addons/files/android-disks.sh
+++ recipes-domd/domd-image-weston/files/meta-xt-prod-extra/recipes-extended/guest-addons/files/android-disks.sh
@@ -7,10 +7,7 @@ RAW_FOLDER="/xt/android/"
 if [ $ROOTFS_TYPE == "nfs" ]
 then
 # rootfs is nfs, expect Android partitions are raw files in /xt/android
-    ln -s /dev/loop0 $ADISKS_FOLDER/system
-    ln -s /dev/loop1 $ADISKS_FOLDER/vendor
-    ln -s /dev/loop2 $ADISKS_FOLDER/misc
-    ln -s /dev/loop3 $ADISKS_FOLDER/userdata
+    ln -s /dev/loop0 $ADISKS_FOLDER/doma
 
     cd $RAW_FOLDER && /xt/scripts/doma_loop_setup.sh || exit 1
 else
@@ -27,10 +24,7 @@ else
     BASE_DEV=`basename "$(readlink -f "/sys/class/block/${BOOT_STORAGE}/..")"`
 # get partition prefix, e.g. "" for sda2 or "p" for mmcblk1p2
     PART_PREFIX=`echo ${BOOT_STORAGE} | eval sed -e 's/${BASE_DEV}//g' | sed 's/[0-9]\+//'`
-    ln -s /dev/${BASE_DEV}${PART_PREFIX}5 $ADISKS_FOLDER/system
-    ln -s /dev/${BASE_DEV}${PART_PREFIX}6 $ADISKS_FOLDER/vendor
-    ln -s /dev/${BASE_DEV}${PART_PREFIX}7 $ADISKS_FOLDER/misc
-    ln -s /dev/${BASE_DEV}${PART_PREFIX}8 $ADISKS_FOLDER/userdata
+    ln -s /dev/${BASE_DEV}${PART_PREFIX}4 $ADISKS_FOLDER/doma
 fi
 
 xenstore-write drivers/disks/status ready
