xen-troops/meta-xt-prod-devel

commit cabe65aa541e150c0727d794a9ee3e8ddb7ea875

qemu: Add qemu-xen to machine overrides while building for QEMU
This eases the task of customizing recipes in the lower Yocto build.

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

recipes-dom0/dom0-image-thin-initramfs/dom0-image-thin-initramfs.bbappend
@@ -67,6 +67,7 @@ add_to_local_conf() {
 
     if [ ! -z ${XT_COMMON_DISTRO_FEATURES_APPEND} ]; then
         base_set_conf_value ${local_conf} DISTRO_FEATURES_append " ${XT_COMMON_DISTRO_FEATURES_APPEND}"
+        base_set_conf_value ${local_conf} MACHINEOVERRIDES_append ":qemu-xen"
     fi
 }
 

commit d93237efdd1ef796246dc9e5677662f5dfce4c9a

Fix adding of qemu to machine overrides
It is not enough to make sure that XT_COMMON_DISTRO_FEATURES_APPEND
exists, but also it needs to be checked against "qemu" in it to be
present.

Fixes: cabe65aa541e ("qemu: Add qemu-xen to machine overrides while building for QEMU")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

recipes-dom0/dom0-image-thin-initramfs/dom0-image-thin-initramfs.bbappend
@@ -73,6 +73,8 @@ add_to_local_conf() {
 
     if [ ! -z ${XT_COMMON_DISTRO_FEATURES_APPEND} ]; then
         base_set_conf_value ${local_conf} DISTRO_FEATURES_append " ${XT_COMMON_DISTRO_FEATURES_APPEND}"
+    fi
+    if echo "${XT_COMMON_DISTRO_FEATURES_APPEND}" | grep -qi "qemu";then
         base_set_conf_value ${local_conf} MACHINEOVERRIDES_append ":qemu-xen"
     fi
 }