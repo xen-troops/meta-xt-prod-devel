xen-troops/meta-xt-prod-devel

commit d67726a9e59c169ed3fdeb1010ce2dacc1770429

qemu: Propagate XT_COMMON_DISTRO_FEATURES_APPEND variable
XT_COMMON_DISTRO_FEATURES_APPEND variable is used for the added distro
features, common for all domains.

NOTE! The string provided with XT_COMMON_DISTRO_FEATURES_APPEND is
directly used as DISTRO_FEATURES_append directive.

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

recipes-dom0/dom0-image-thin-initramfs/dom0-image-thin-initramfs.bbappend
@@ -64,6 +64,10 @@ add_to_local_conf() {
     base_update_conf_value ${local_conf} PREFERRED_VERSION_xen "4.14.0+git\%"
 
     base_update_conf_value ${local_conf} XT_GUESTS_INSTALL "${XT_GUESTS_INSTALL}"
+
+    if [ ! -z ${XT_COMMON_DISTRO_FEATURES_APPEND} ]; then
+        base_set_conf_value ${local_conf} DISTRO_FEATURES_append " ${XT_COMMON_DISTRO_FEATURES_APPEND}"
+    fi
 }
 
 python do_configure_append() {

recipes-domu/domu-image-weston/domu-image-weston.bbappend
@@ -91,6 +91,10 @@ configure_versions_rcar() {
 
     # set default timezone to Las Vegas
     base_update_conf_value ${local_conf} DEFAULT_TIMEZONE "US/Pacific"
+
+    if [ ! -z ${XT_COMMON_DISTRO_FEATURES_APPEND} ]; then
+        base_set_conf_value ${local_conf} DISTRO_FEATURES_append " ${XT_COMMON_DISTRO_FEATURES_APPEND}"
+    fi
 }
 
 python do_configure_append_rcar() {

commit 8890f367da82bf512c95a55fe7f22e7ab66e680d

Fix multiword XT_COMMON_DISTRO_FEATURES_APPEND propagation
The build breaks on comparisons if XT_COMMON_DISTRO_FEATURES_APPEND
is a multiword string.

Fixes: d67726a9e59c ("qemu: Propagate XT_COMMON_DISTRO_FEATURES_APPEND variable")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

recipes-dom0/dom0-image-thin-initramfs/dom0-image-thin-initramfs.bbappend
@@ -71,7 +71,7 @@ add_to_local_conf() {
 
     base_update_conf_value ${local_conf} XT_GUESTS_INSTALL "${XT_GUESTS_INSTALL}"
 
-    if [ ! -z ${XT_COMMON_DISTRO_FEATURES_APPEND} ]; then
+    if [ ! -z "${XT_COMMON_DISTRO_FEATURES_APPEND}" ]; then
         base_set_conf_value ${local_conf} DISTRO_FEATURES_append " ${XT_COMMON_DISTRO_FEATURES_APPEND}"
     fi
     if echo "${XT_COMMON_DISTRO_FEATURES_APPEND}" | grep -qi "qemu";then

recipes-domu/domu-image-weston/domu-image-weston.bbappend
@@ -92,7 +92,7 @@ configure_versions_rcar() {
     # set default timezone to Las Vegas
     base_update_conf_value ${local_conf} DEFAULT_TIMEZONE "US/Pacific"
 
-    if [ ! -z ${XT_COMMON_DISTRO_FEATURES_APPEND} ]; then
+    if [ ! -z "${XT_COMMON_DISTRO_FEATURES_APPEND}" ]; then
         base_set_conf_value ${local_conf} DISTRO_FEATURES_append " ${XT_COMMON_DISTRO_FEATURES_APPEND}"
     fi
 }